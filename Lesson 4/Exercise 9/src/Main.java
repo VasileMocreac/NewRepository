498599b1734ee49ea5049c818eb2edaba0ad36b7
Mocreac Vasile <vasile.mocreac@nrg.md>
2020-07-03 13:11:23 +0300

    lesson 4 homework

--- Lesson 4/Exercise 9/src/Main.java
+++ Lesson 4/Exercise 9/src/Main.java
@@ -7,9 +7,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
+        boolean b1 = (0 >= a || 0 >= b || 0 >= c);
+        System.out.println(b1);
 
-        System.out.println(a / b );
+        int e = scanner.nextInt();
+        int f = scanner.nextInt();
+        int g = scanner.nextInt();
+
+        boolean b2 = (0 >= e || 0 >= f || 0 >= g);
+        System.out.println(b2);
 
 
     }
