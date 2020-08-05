11d6e19587335d7daa92ea8fcb0a538c296c0d55
Mocreac Vasile <vasile.mocreac@nrg.md>
2020-08-05 17:21:04 +0300

    lesson 13 hw

---
+++ Lesson 11/Exercise 1/src/Primitive.java
@@ -0,0 +1,8 @@
+
+
+class Primitive {
+    public static boolean toPrimitive(Boolean b) {
+        return b != null && b;
+    }
+}
+
--- TicTacToe/Stage 1.5/src/Main.java
+++ TicTacToe/Stage 1.5/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-		System.out.println("X O X");
-		System.out.println("O X O");
-		System.out.println("X X O");
+        System.out.println("---------");
+		System.out.println("| O _ O |");
+		System.out.println("| X X O |");
+		System.out.println("| _ X X |");
+        System.out.println("---------");
 
 
 
