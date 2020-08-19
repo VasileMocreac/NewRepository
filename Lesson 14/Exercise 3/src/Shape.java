ebdba8b3506e8713a3dbf006ec74fb74eda5e369
Mocreac Vasile <vasile.mocreac@nrg.md>
2020-08-19 12:10:38 +0300

    lesson 15 ex.1

--- Lesson 14/Exercise 3/src/Shape.java
+++ Lesson 14/Exercise 3/src/Shape.java
@@ -1,2 +1,4 @@
-public class Shape {
+public abstract class Shape {
+    abstract double getPerimeter();
+    abstract double getArea();
 }
---
+++ Lesson 14/Exercise 3/src/Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle extends Shape {
+    protected double a;
+    protected double b;
+    protected double c;
+
+    @Override
+    double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    double getArea() {
+        return 0;
+    }
+}
+
+
